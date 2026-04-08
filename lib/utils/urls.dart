e4667fcb1dd4e2eaead7fb70e81d2fdef9aaeeab
MD. ATIKUR RAHMAN <margaretjconn528@gmail.com>
2026-04-08 14:49:11 +0600

    fixed url

--- lib/utils/urls.dart
+++ lib/utils/urls.dart
@@ -1,14 +1,18 @@
-class Urls{
+class Urls {
   static String _baseUrl = 'http://35.73.30.144:2005/api/v1';
   static String SignUpURL = '$_baseUrl/registration';
   static String LoginUrl = '$_baseUrl/login';
   static String AddTaskURL = '$_baseUrl/createTask';
   static String TaskCountURL = '$_baseUrl/taskStatusCount';
   static String ProfileUpdateURL = '$_baseUrl/profileUpdate';
-  static String TaskByStatusURL(String status) => '$_baseUrl/listTaskByStatus/$status';
+  static String TaskByStatusURL(String status) =>
+      '$_baseUrl/listTaskByStatus/$status';
   static String DeleteTaskURL(String id) => '$_baseUrl/deleteTask/$id';
-  static String ChangeStatusURL(String id, String status) => '$_baseUrl/updateTaskStatus/$id/$status';
-  static String RecoverVerifyEmail(String email) => '$_baseUrl/RecoverVerifyEmail/$email';
-  static String RecoverVerifyOTP(String email, String otp) => '$_baseUrl/RecoverVerifyOTP/$email/$otp';
-  static String RecoverResetPass = '$_baseUrl/RecoverResetPass';
+  static String ChangeStatusURL(String id, String status) =>
+      '$_baseUrl/updateTaskStatus/$id/$status';
+  static String RecoverVerifyEmail(String email) =>
+      '$_baseUrl/RecoverVerifyEmail/$email';
+  static String RecoverVerifyOTP(String email, String otp) =>
+      '$_baseUrl/RecoverVerifyOTP/$email/$otp';
+  static String RecoverResetPass = '$_baseUrl/RecoverResetPassword';
 }
